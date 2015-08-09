2ecc4d998ee741fe48c5f8702aec912a57a06c5a
Sandeep Wadhwa <sandeep.sajiru@gmail.com>
2015-08-09 00:05:03 -0700

    Added test for FileUpload in themes

---
+++ src/FileUpload.java
@@ -0,0 +1,51 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+class GlobalConstants {
+
+	public static final String LOGIN_PAGE_URL = "http://127.0.0.1/wordpress/wp-admin";
+	public static final String USER_NAME = "sandeep";
+	public static final String PASSWORD = "sandeep";
+	public static final String THEME_UPLOAD_PAGE_URL = "http://127.0.0.1/wordpress/wp-admin/theme-install.php?upload";
+	public static final CharSequence THEME_ZIP_FILE_PATH = System.getProperty("user.dir")+"\\TestData\\Tessaract.zip";
+	
+}
+
+public class FileUpload {
+
+	@Test
+	public void run() throws InterruptedException {
+		System.out.println(System.getProperty("user.dir"));
+		WebDriver fd  = new FirefoxDriver();
+		fd.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		fd.get(GlobalConstants.LOGIN_PAGE_URL);
+		
+		fd.findElement(By.name("log")).sendKeys(GlobalConstants.USER_NAME);
+		fd.findElement(By.name("pwd")).sendKeys(GlobalConstants.PASSWORD);
+		
+		fd.findElement(By.name("wp-submit")).click();
+		
+		fd.findElement(By.linkText("Customize Your Site"));
+		
+		fd.navigate().to(GlobalConstants.THEME_UPLOAD_PAGE_URL);
+		
+		fd.findElement(By.name("themezip")).sendKeys(GlobalConstants.THEME_ZIP_FILE_PATH);
+		
+		WebElement installNow =  fd.findElement(By.name("install-theme-submit"));
+		System.out.println(installNow.isEnabled());
+		if(installNow.isEnabled())
+		{
+			installNow.submit();
+		}
+		
+		Thread.sleep(2000);
+		fd.quit();
+		
+	}
+}
